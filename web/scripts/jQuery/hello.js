e46fecd56fc89b4d68a7c10bf93f9e50418b85cd
stamoulisR <77421783+stamoulisR@users.noreply.github.com>
2021-03-16 14:33:58 -0700

    pushme: fixing files

--- web/scripts/jQuery/hello.js
+++
@@ -1,4 +0,0 @@
-function insertHello() {
-    $("#demo").text("Hello from jQuery!")
-    console.log("Hello to the console!")
-  }
--- web/scripts/js/hello.js
+++
@@ -1,4 +0,0 @@
-function insertHello() {
-    document.getElementById("demo").innerHTML = "Hello from JS!"
-    console.log("Hello to the console!")
-  }
